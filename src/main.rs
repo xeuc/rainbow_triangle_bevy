e7c34418a5fad41821f5b69786c6ac5dd73cc634
xeuc <80948867+xeuc@users.noreply.github.com>
2024-03-17 14:45:28 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use bevy::{
+    prelude::*,
+    sprite::{MaterialMesh2dBundle, Mesh2dHandle},
+};
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    // Make the scene visible
+    commands.spawn(Camera2dBundle::default());
+
+    // Defeine the triangle form
+    let mut mesh = Mesh::from(Triangle2d::default());
+
+    // Define the colors of corners
+    let vertex_colors: Vec<[f32; 4]> = vec![
+        Color::RED.as_rgba_f32(),
+        Color::GREEN.as_rgba_f32(),
+        Color::BLUE.as_rgba_f32(),
+    ];
+    mesh.insert_attribute(Mesh::ATTRIBUTE_COLOR, vertex_colors);
+    let mesh_handle: Mesh2dHandle = meshes.add(mesh).into();
+    
+    // Render the triangle
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: mesh_handle.clone(),
+        transform: Transform::from_scale(Vec3::splat(512.)),
+        material: materials.add(ColorMaterial::default()),
+        ..default()
+    });
+}
+
